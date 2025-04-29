6752992b8ac7f994f86653059dafabff01869bc6
Szymon Kownacki <szymon.kownacki@gmail.com>
2025-04-29 09:37:54 +0200

    Add docs for web analytics permission requirements

--- infra/analytics.tf
+++ infra/analytics.tf
@@ -1,4 +1,5 @@
-resource "cloudflare_web_analytics_site" "example" {
+# requires separate READ and EDIT permissions for account analytics, as well as READ zone analytics
+resource "cloudflare_web_analytics_site" "analytics" {
   account_id   = data.cloudflare_zone.zone.account_id
   zone_tag     = data.cloudflare_zone.zone.zone_id
   auto_install = true
